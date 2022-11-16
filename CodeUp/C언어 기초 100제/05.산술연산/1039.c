d41ad9f4a34f62ec6751c7fc12ad812b44414d02
Dongil <kdi25144@gmail.com>
2022-11-16 16:41:58 +0900

    Update

---
+++ CodeUp/C언어 기초 100제/05.산술연산/1039.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	long int n1, n2;
+	scanf("%ld%ld", &n1, &n2);
+	printf("%ld", n1+n2);
+
+	return 0;
+}
--- CodeUp/C언어 기초 100제/05.산술연산/1046.c
+++ CodeUp/C언어 기초 100제/05.산술연산/1046.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	long int n;
-    scanf("%ld", &n);
-    printf("%ld", n);
-
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	printf("%d \n", a + b + c);
+	printf("%.1f", (a+b+c)/3.0);
 	return 0;
 }
