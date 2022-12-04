c9918e4d4c0968d2780476c0bb4bf41c8673f8ca
Dongil <kdi25144@gmail.com>
2022-12-04 21:44:34 +0900

    Update

---
+++ 01-1 input/output/03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int num;
+	
+	std::cout << "숫자 입력: ";
+	std::cin >> num;
+
+	std::cout << "입력하신 숫자로 구구단을 출력하겠습니다." << std::endl;
+
+	for (int i = 1; i < 10; i++) {
+		std::cout << num << " * " << i << " = " << num * i << std::endl;
+	}
+
+	return 0;
+}
