a5b49fb55e218386291a7d878d5c43a0ffff1cb7
Dongil <kdi25144@gmail.com>
2022-12-05 12:23:24 +0900

    Update

---
+++ 01-3 Default_Value/01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int BoxVolume(int length, int width, int height) {
+	return length * width * height;
+}
+
+int BoxVolume(int length, int width) {
+	return length * width;
+}
+
+int BoxVolume(int length) {
+	return length;
+}
+
+int BoxVolume() {
+	return 0;
+}
+
+
+int main() {
+	std::cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << std::endl;
+	std::cout << "[5, 5, D] : " << BoxVolume(5, 5) << std::endl;
+	std::cout << "[7, D, D] : " << BoxVolume(7) << std::endl;
+//  std::cout << "[D, D, D] : " << BoxVolume() << std::endl;
+
+	return 0;
+}
