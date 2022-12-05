df4793003ed3df9e38f1313ec25ca2f21fcd1b3b
Dongil <kdi25144@gmail.com>
2022-12-05 13:21:34 +0900

    Update

---
+++ 01-4 File_Division/main.cpp
@@ -0,0 +1,7 @@
+#include "헤더.h"
+
+int main(void) {
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	return 0;
+}
---
+++ 01-4 File_Division/소스.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "헤더.h"
+
+void BestComImpl::SimpleFunc(void) {
+	std::cout << "BestCom이 정의함 함수" << std::endl;
+}
+void ProgComImpl::SimpleFunc(void) {
+	std::cout << "ProgCom이 정의함 함수" << std::endl;
+}
