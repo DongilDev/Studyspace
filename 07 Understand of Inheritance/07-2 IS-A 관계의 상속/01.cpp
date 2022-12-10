4cbe0c5e43c626fb797efc6197a24f2e7986b171
Dongil <kdi25144@gmail.com>
2022-12-10 22:04:25 +0900

    Update

---
+++ 07 Understand of Inheritance/07-2 IS-A 관계의 상속/01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+	int width; // 가로
+	int height; // 세로
+public:
+	Rectangle(int _width, int _height) : width(_width), height(_height) {}
+	
+	void ShowAreaInfo() {
+		cout << "면적: " << width * height << endl;
+	}
+};
+
+class Square : public Rectangle {
+	int length; // 길이
+public:
+	Square(int _length) : Rectangle(_length, _length), length(_length) {}
+};
+
+int main() {
+	Rectangle rec(4, 3);
+	rec.ShowAreaInfo();
+
+	Square sqr(7);
+	sqr.ShowAreaInfo();
+
+	return 0;
+}
