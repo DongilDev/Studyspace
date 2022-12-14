013a8c01076950b4fb823d3b0aad100e03b797d1
Dongil <kdi25144@gmail.com>
2022-12-14 21:43:55 +0900

    Update

---
+++ 11 연산자 오버로딩 2/11-4 OOP 단계별 프로젝트 08단계/Account.h
@@ -0,0 +1,20 @@
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+class Account {
+	int accID;
+	int balance;
+	char* cusName;
+
+public:
+	Account(int id, int money, char* name);
+	Account(const Account& ref);
+	Account& operator=(const Account& ref);
+
+	int GetAccID() const;
+	virtual void Deposit(int money);
+	int Withdraw(int money);
+	void ShowAccInfo() const;
+	~Account();
+};
+#endif
